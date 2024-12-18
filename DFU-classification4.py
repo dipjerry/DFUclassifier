77677817b8adad730020acc2f875e622fb8ac185
R.Basudev <152674849+bdev-rohan@users.noreply.github.com>
2024-12-18 21:38:23 +0530

    Add files via upload
    classifictaion-model4

---
+++ DFU-classification4.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+# Initialize a YOLO model for classification
+model = YOLO('yolov8s-cls.pt')  # Use pre-trained YOLOv8 classification model
+
+# Train the model
+model.train(
+    data='/home/rohanb/Documents/mca/ML Mini Project/provided_datasets/DFUC2021_classification',  # Path to your dataset
+    epochs=100,
+    imgsz=224,
+    batch=16
+)
+
