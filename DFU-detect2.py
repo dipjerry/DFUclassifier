f4b0f72a4fae06fa144570e90a7a253b72d9fb36
R.Basudev <152674849+bdev-rohan@users.noreply.github.com>
2024-12-18 21:45:55 +0530

    Add files via upload
    code for detect-model1,2,3,4

---
+++ DFU-detect2.py
@@ -0,0 +1,14 @@
+# DFU2.py
+
+from ultralytics import YOLO
+
+# Load a YOLOv8 model
+model = YOLO('yolov8s.pt')  # We can use yolov8s.pt or others
+
+# Train the model
+model.train(
+    data='/home/rohanb/Documents/mca/ML Mini Project/dfu/dataset.yaml',
+    epochs=50, # configuration of parameters for training
+    imgsz=224,
+    batch=32
+)
